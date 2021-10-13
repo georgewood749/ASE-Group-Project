0d9bc872b5d51884b8197decdbab1ce63a1667b0
bobo-byte <57590070+bobo-byte@users.noreply.github.com>
2021-10-13 18:44:43 +0100

    added: useLocation hook file for getting device location using expo SDK. MapView Component added. Added assets folder

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, StyleSheet, ScrollView, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import useLocation from "./hooks/useLocation";
 
 export default function App() {
+  const [location, errorMsg] = useLocation();
+
+  let text = "Waiting..";
+  let coords = {};
+  if (errorMsg) {
+    // if error then text displayed will contain error.
+    text = errorMsg;
+  } else if (location) {
+    /*
+     * JSON.stringify with 2 as the 3rd argument.
+     * This will make the returned JSON string have 2 spaces for indentation at each level.
+     */
+    text = JSON.stringify(location, null, 2);
+    coords = location.coords;
+  }
+
+  const { latitude, longitude } = coords;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView>
+        <Text style={styles.paragraph}>{text}</Text>
+      </ScrollView>
+      {location ? (
+        <MapView
+          initialRegion={{
+            latitude,
+            longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          style={styles.map}
+        >
+          <Marker coordinate={{ latitude, longitude }} />
+        </MapView>
+      ) : (
+        <View />
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  paragraph: {
+    fontSize: 20,
+    flex: 1,
+    padding: 20,
+  },
+  map: {
+    width: "100%",
+    height: "50%",
   },
 });
---
+++ hooks/useLocation.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import {requestForegroundPermissionsAsync, getCurrentPositionAsync} from 'expo-location';
+
+/**
+ * Function to hook location request. 
+ * Read more about it on: 
+ * https://docs.expo.dev/versions/latest/sdk/location
+ */
+
+export default () => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    // when the component is mounting we ask for permission.
+    (async () => {
+      let { status } = await requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  return [location, errorMsg];
+};
